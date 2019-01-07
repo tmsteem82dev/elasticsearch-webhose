cbe720a6a7087f79d7a3746e187828715601a432
tims <tsteeman@jibes.nl>
2019-01-07 15:01:11 +0100

    changed component versions, added code to ingest dummy users

--- data-ingester/ingest.py
+++ data-ingester/ingest.py
@@ -29,20 +29,57 @@ doc = {
     'timestamp': datetime.now(),
 }
 
+def dummy_data():
+    res = es.index(index="test-index", doc_type="tweet", id=1, body=doc)
 
-res = es.index(index="test-index", doc_type="tweet", id=1, body=doc)
+    logger.info("result: " + str(res['result']))
 
-logger.info("result: " + str(res['result']))
+    res = es.get(index="test-index", doc_type="tweet", id=1)
 
-res = es.get(index="test-index", doc_type="tweet", id=1)
+    logger.info("es.get result: " + str(res["_source"]))
 
-logger.info("es.get result: " + str(res["_source"]))
+    es.indices.refresh(index="test-index")
 
-es.indices.refresh(index="test-index")
+    res = es.search(index="test-index", body={"query": {"match_all": {}}})
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
+    logger.info("Got %d hits:" % res["hits"]["total"])
 
-logger.info("Got %d hits:" % res["hits"]["total"])
+    for hit in res["hits"]["hits"]:
+        logger.info("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
 
-for hit in res["hits"]["hits"]:
-    logger.info("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+users = [{
+    'name': 'harry',
+    'article_likes': ['Planschade Rijksenergieprojecten', 'Maatschappelijk Verantwoord Innoveren', 'Aardgasvrij']
+
+},
+{
+    'name': 'Lenz',
+    'article_likes': ['Planschade Rijksenergieprojecten', 'Banken met groen fonds', 'Aardgasvrij']
+},
+{
+    'name': 'Reit',
+    'article_likes': ['Carbon capture', 'Bank falliet', 'Duurzame energie industrie', 'Groene energie opwekken']
+},
+{
+    'name': 'Vicky',
+    'article_likes': ['Met een waterrad duurzame energie in een handomdrai', 'Aardgasvrij', 'Vrije aardgas hoopjes', 'Waarom aardgasvrij']
+},
+{
+    'name': 'Mumsfeld',
+    'article_likes': ['Maatschappelijk Verantwoord Innoveren', 'Blackhole looms', 'From the beyond', 'Spacestation on fire', 'The 2000 things you can do to...']
+},
+{
+    'name': 'Mumsfeld',
+    'article_likes': ['Blackhole looms']
+}]
+
+def remove_index(index_name):
+    logger.info("Deleting index %s" % index_name)
+    es.indices.delete(index=index_name, ignore=[400, 404])
+
+def ingest_dummy_users():
+    logger.info("Inserting user data")
+
+    for user in users:
+        res = es.index(index="users-meta", doc_type="user-metadata", body=user)
+
